8cb6a8ca4151d4c5c0877746576dd9e5710a3e04
Alec O'Connor <atocon@bu.edu>
2021-11-29 11:51:12 -0500

    Basic functionality completed.

--- assignment4/validation.js
+++ assignment4/validation.js
@@ -1,28 +1,30 @@
-document.getElementById('cuisineForm').addEventListener('submit', e => {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
+document.getElementById("cuisineForm").addEventListener("submit", e => {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
     if (firstName.length < 2) {
-        alert("Error: The entered first name must contain two (2) or more characters.");
+        alert("Error: First name must contain two (2) or more characters.");
         e.preventDefault();
     } 
     if (lastName.length < 2) {
-        alert("Error: The entered last name must contain two (2) or more characters.");
+        alert("Error: Last name must contain two (2) or more characters.");
         e.preventDefault();
     }
 
-    //const alpha = /^[A-Za-z]+$/;
-    //if (!firstName.match(alpha)) {
-        //alert("Error: The entered first name must only contain alphanumeric characters.");
-        //e.preventDefault();
-    //}
-    //if (!lastName.match(alpha)) {
-        //alert("Error: The entered last name must only contain alphanumeric characters.");
-        //e.preventDefault();
-    //}
+    const alpha = /^[A-Za-z]+$/;
+    if (!firstName.match(alpha)) {
+        alert("Error: First name must only contain alphabetical characters.");
+        e.preventDefault();
+    }
+    if (!lastName.match(alpha)) {
+        alert("Error: Last name must only contain alphabetical characters.");
+        e.preventDefault();
+    }
 
-    //const facilitator = document.getElementById("Facilitator").value;
-    //if (facilitator !== "Josh" || "Chris" || "Fazil" || "Christian") {
-        //alert("Error: The entered facilitator name is not valid.");
-        //e.preventDefault();
-    //}
+    const facilitator = document.getElementById("facilitator").value;
+    if (facilitator === "Chris" || facilitator === "Josh" || facilitator === "Christian" || facilitator === "Fazil") {
+        // Do nothing.
+    } else {
+        alert("Error: Facilitator name is not valid.");
+        e.preventDefault();
+    }
 });
