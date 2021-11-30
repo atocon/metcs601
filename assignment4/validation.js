38f0f8f853dfbe4d62c5f845f2624bbbd619605c
Alec O'Connor <atocon@bu.edu>
2021-11-29 21:46:56 -0500

    Completed form.

--- assignment4/validation.js
+++ assignment4/validation.js
@@ -2,29 +2,40 @@ document.getElementById("cuisineForm").addEventListener("submit", e => {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     if (firstName.length < 2) {
-        alert("Error: First name must contain two (2) or more characters.");
+        document.getElementById("firstName").style.borderColor = "red";
+        document.getElementById("firstNameError").innerHTML = 
+        "Error: First name must contain two (2) or more characters.";
         e.preventDefault();
     } 
     if (lastName.length < 2) {
-        alert("Error: Last name must contain two (2) or more characters.");
+        document.getElementById("lastName").style.borderColor = "red";
+        document.getElementById("lastNameError").innerHTML = 
+        "Error: Last name must contain two (2) or more characters.";
         e.preventDefault();
     }
 
     const alpha = /^[A-Za-z]+$/;
     if (!firstName.match(alpha)) {
-        alert("Error: First name must only contain alphabetical characters.");
+        document.getElementById("firstName").style.borderColor = "red";
+        document.getElementById("firstNameError").innerHTML = 
+        "Error: First name must only contain alphabetical characters.";
         e.preventDefault();
     }
     if (!lastName.match(alpha)) {
-        alert("Error: Last name must only contain alphabetical characters.");
+        document.getElementById("lastName").style.borderColor = "red";
+        document.getElementById("lastNameError").innerHTML = 
+        "Error: Last name must only contain alphabetical characters.";
         e.preventDefault();
     }
 
     const facilitator = document.getElementById("facilitator").value;
-    if (facilitator === "Chris" || facilitator === "Josh" || facilitator === "Christian" || facilitator === "Fazil") {
+    if (facilitator === "Chris" || facilitator === "Josh" || facilitator === 
+        "Christian" || facilitator === "Fazil") {
         // Do nothing.
     } else {
-        alert("Error: Facilitator name is not valid.");
+        document.getElementById("facilitator").style.borderColor = "red";
+        document.getElementById("facilitatorNameError").innerHTML = 
+        "Error: Facilitator name is not valid.";
         e.preventDefault();
     }
 });
